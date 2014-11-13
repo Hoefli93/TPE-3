0901198999eb7eed0153ca31e443e631b9fbce61
MaxZ5 <max.zittel@web.de>
2014-11-13 14:26:35 +0100

    associative Array Vorschlag

--- TPE3/src/tpe/uib/ü3/hs_ma/AssociativeArray.java
+++ TPE3/src/tpe/uib/ü3/hs_ma/AssociativeArray.java
@@ -10,3 +10,33 @@ System.out.println("pisser");
 	}
 
 }
+
+/*
+ * import java.util.HashMap;
+import java.util.Map;
+ 
+HashMap<String, String> Data = new HashMap<String, String>();
+ 
+Data .put("id", "1");
+Data .put("firstname", "muktesh");
+Data .put("lastname", "kumar");
+
+or We can also create multi-level associative array in java or android using map.
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+ 
+List<HashMap<String, String>> assArray= new ArrayList<HashMap<String, String>>();
+ 
+HashMap<String, String> Data= new HashMap<String, String>();
+ 
+Data .put("id", "1");
+Data .put("firstname", "muktesh");
+Data .put("lastname", "kumar");
+assArray.add("0", Data);
+
+
+ */
+
