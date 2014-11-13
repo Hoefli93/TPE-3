4a1cda7c27ff3642654f955dcca8e5b0d892f296
1320733 <1320733@141.19.7.22>
2014-11-13 13:59:42 +0100

    3te

--- TPE3/src/tpe/uib/main.java
+++ TPE3/src/tpe/uib/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hallo");
-System.out.println("Sinan sucks");
+System.out.println("i love Sinan ");
 	}
 
 }
